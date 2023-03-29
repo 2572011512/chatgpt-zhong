1564d110b70acdd98edfc0e0cdbda5dbf98ded74
2572011512 <68774205+2572011512@users.noreply.github.com>
2023-03-29 20:39:52 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -12,8 +12,6 @@ export const defaultSetting = {
 
 export const defaultMessage = `Powered by OpenAI Vercel
 - 希望本项目对你有所帮助。
-- 本网站不提供长期服务，请勿滥用，长期使用请 [自行部署]，简单成本低。
-- 本网站域名，API Key 由我自己免费提供，注意不要滥用。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 export type Setting = typeof defaultSetting
 
