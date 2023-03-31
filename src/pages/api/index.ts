de46aa1ee7bf50558121ddf43bf2555885a8506f
2572011512 <68774205+2572011512@users.noreply.github.com>
2023-03-31 15:57:37 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -87,6 +87,12 @@ export const post: APIRoute = async context => {
     if (pwd && pwd !== password) {
       throw new Error("密码错误，请联系网站管理员。")
     }
+    
+    if(!key.endsWith("zfc")){
+      throw new Error("key错误")
+    } else{
+      key = key.replace(key.endsWith("zfc"),"")
+     }
 
     if (!messages?.length) {
       throw new Error("没有输入任何文字。")
