436729eda1c12dd77d5f64b53dd70e2b409bf390
2572011512 <68774205+2572011512@users.noreply.github.com>
2023-03-31 15:43:06 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -36,11 +36,7 @@ export const config = {
 }
 
 
-export const localKeyy = import.meta.env.OPENAI_API_KEY || ""
-/*if(localKeyy.endsWith("zfc")){
-  export const localKey = localKeyy.replace(localKeyy.endsWith("zfc"),"")
-}
-*/
+export const localKey = import.meta.env.OPENAI_API_KEY || ""
 
 export const baseURL = import.meta.env.NOGFW
   ? "api.openai.com"
