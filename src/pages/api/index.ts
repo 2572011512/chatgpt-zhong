6a5b2651c39c1ef8e1b58066d3b3c81ce50249a5
2572011512 <68774205+2572011512@users.noreply.github.com>
2023-04-01 12:59:52 +0800

    Update index.ts

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -113,7 +113,7 @@ export const post: APIRoute = async context => {
     
     const apiKey = randomKey(splitKeys(key))
       
-    if (!apiKey || !apiKey.endsWith("gB")) throw new Error("没有填写 OpenAI API key，或者 key 填写错误。")
+    if (!apiKey) throw new Error("没有填写 OpenAI API key，或者 key 填写错误。")
 
     const tokens = messages.reduce((acc, cur) => {
       const tokens = countTokens(cur.content)
